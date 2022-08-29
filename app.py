f05201feaac42baf43d83efbc05336e2e9b530d4
nictit <87221489+nictit@users.noreply.github.com>
2022-08-29 09:40:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,13 @@ app = flask.Flask(__name__)
 @app.route("/", methods=['POST'])
 def index():
     chat_id, user_name = TGapi.WH_analyse(flask.request.json)
-    #msg = ShipFinder.main()
-    msg = 'hi'
+    msg = ShipFinder.main()
     print(msg)
     TGapi.sendMsg(msg, chat_id, user_name)
     return 'hi'
 
 if __name__ == '__main__':
     TGapi.deleteWH()
-    TGapi.setWH('https://fortesty.herokuapp.com/')
+    TGapi.setWH('https://fortesty.herokuapp.com/5048232576:AAHKQXWuVI-KIFQOEsDEizTGo9A1Ahjk4cw')
     print('ready')
-    app.run(host="https://fortesty.herokuapp.com/", port=int(os.environ.get("PORT", 8443)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8443)))
